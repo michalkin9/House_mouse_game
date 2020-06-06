0c087fe05316c94421eb31c6bc7c240ff4b14628
Michal Yelkin <michalkin9@gmail.com>
2020-06-07 00:42:00 +0300

    jump and eat

--- game/Assets/Scripts/playerMovment.cs
+++ game/Assets/Scripts/playerMovment.cs
@@ -5,53 +5,88 @@ using UnityEngine;
 public class playerMovment : MonoBehaviour
 {
     public CharacterController controller;
-    public float speed = 12f;
-    float gravity = 9.81f;
+    public float speed = 5f;
+    public float jumpHeight = 3f;
+    float gravity = -9.81f;
     Vector3 velocity;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        controller = GetComponent<CharacterController>();
-    }
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+    
+
 
     // Update is called once per frame
     void Update()
     {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            Debug.Log("isGRounded true");
+            velocity.y = -2f;
+        }
+
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        velocity.x = x * speed;
-        velocity.z = z * speed;
 
-        if (!controller.isGrounded)
-        {
-            velocity.y -= gravity * Time.deltaTime;
-
-        }
-
-        velocity = transform.TransformDirection(velocity);
-
+       
 
         Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime );
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space) ) // לא עובד עם && isGrounded לבדוק למה
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
 
     }
 
     private void OnCollisionEnter(Collision collision)
     {
 
-        Debug.Log("collied with food");
+        if (collision.gameObject.name == "floor")
+        {
+          Debug.Log("collied with floor");
+            isgrounded1 = true;
+        }
 
-       // if (collision.gameObject.name == "food")
-      //  {
-      //      Debug.Log("collied with food");
-       //     Destroy(collision.gameObject);
-      //  }
+        if (collision.gameObject.name == "foodToEat")
+        {
+            Debug.Log(collision.gameObject.name);
+            Debug.Log("collied with 2 food");
+            Destroy(collision.gameObject);
+       }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("trigger with food");
-    }
+
+
+
 }
+
+//     float x = Input.GetAxis("Horizontal");
+//float z = Input.GetAxis("Vertical");
+
+//velocity.x = x* speed;
+//velocity.z = z* speed;
+
+   //     if (!controller.isGrounded)
+   //     {
+   //         velocity.y -= gravity* Time.deltaTime;
+
+   //     }
+
+   //     velocity = transform.TransformDirection(velocity);
+//
+
+   //     Vector3 move = transform.right * x + transform.forward * z;
+//controller.Move(move* speed * Time.deltaTime );
