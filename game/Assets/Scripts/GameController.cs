63ab6e8e0be9b83926360561247b972926e0c267
Michal Yelkin <michalkin9@gmail.com>
2020-06-18 14:39:12 +0300

    commit

---
+++ game/Assets/Scripts/GameController.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    bool isCollied;
+    bool isPickedObject;
+    bool isFinishedLevel;
+    public float Health = 0.5f;
+    public Image HealthFillBar;
+    public GameObject CompletePanel , GameOverPanel;
+    public GameObject TrapedPanel;
+
+    private GameObject SelectedObject;
+    public Material NormalMaterial, PickedMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthFillBar.fillAmount = Health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) && isCollied)
+        {
+            SelectedObject.transform.SetParent(gameObject.transform);
+            SelectedObject.GetComponent<BoxCollider>().enabled = false;
+            SelectedObject.GetComponent<MeshRenderer>().material = PickedMaterial;
+            isCollied = false;
+            isPickedObject = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Return)  && isPickedObject)
+        {
+            SelectedObject.transform.SetParent(null);
+            SelectedObject.GetComponent<BoxCollider>().enabled = true;
+           // Invoke("EnableBoxCollider", 1f);
+            SelectedObject.GetComponent<MeshRenderer>().material = NormalMaterial;
+            isPickedObject = false;
+        }
+    }
+    private void EnableBoxCollider()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        if (Health > 0 && !isFinishedLevel)
+        {
+            Health = Health - 0.0001f;
+            CheckHealth();
+        }
+
+        //HelperArrow.transform.LookAt(LevelObjects[PlayerPrefs.GetInt("SelectedLevel") - 1].transform.GetChild(1).transform);
+    }
+    void CheckHealth()
+    {
+        if (Health > 0)
+        {
+            HealthFillBar.fillAmount = Health;
+        }
+        else
+        {
+            GameOver();
+            isFinishedLevel = true;
+        }
+    }
+    private void GameOver()
+    {
+        GameOverPanel.SetActive(true);
+        Invoke("Restart", 2f);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.name == "floor")
+        {
+            Debug.Log("collied with floor");
+
+        }
+
+        if (collision.gameObject.CompareTag("food")) //collision.gameObject.name == "foodToEat"
+        {
+            Health = Health + .35f;
+            if (Health >= 1.0f && SceneManager.GetActiveScene().buildIndex != 3)
+            {
+                CompletePanel.SetActive(true);
+                Invoke("NextLevel", 2f);
+            }
+            Debug.Log("collied with 2 food");
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.CompareTag("trap"))
+        {
+            Destroy(collision.gameObject);
+            TrapedPanel.SetActive(true);
+            Invoke("Restart", 2f);
+        }
+        if (collision.gameObject.CompareTag("moveable") && !isPickedObject)
+        {
+            isCollied = true;
+            SelectedObject = collision.gameObject;
+        }
+        if (collision.gameObject.CompareTag("FoodOnTable"))
+        {
+            CompletePanel.SetActive(true);
+            Invoke("Home", 2f);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("moveable") && !isPickedObject)
+        {
+            isCollied = false;
+            SelectedObject = null;
+        }
+    }
+    public void NextLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void Home()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+}
--- game/Assets/Scripts/playerMovment.cs
+++ game/Assets/Scripts/playerMovment.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class playerMovment : MonoBehaviour
 {
@@ -10,25 +12,28 @@ public class playerMovment : MonoBehaviour
     float gravity = -9.81f;
     Vector3 velocity;
     bool isGrounded;
-
+    bool isCollied;
+    bool isPickedObject;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     public float Health = 1;
+    public Image FillBar;
+    public GameObject CompletePanel;
+    public GameObject TrapedPanel;
 
-
-
-
+    private GameObject SelectedObject;
+    public Material NormalMaterial, PickedMaterial;
     // Update is called once per frame
     void Update()
     {
 
        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
         {
-          //  Debug.Log("isGRounded true");
+            //  Debug.Log("isGRounded true");
             velocity.y = -2f;
         }
 
@@ -51,7 +56,19 @@ public class playerMovment : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
-
+        if (Input.GetKeyDown(KeyCode.Return) && isCollied)
+        {
+            SelectedObject.transform.SetParent(gameObject.transform);
+            SelectedObject.GetComponent<MeshRenderer>().material = PickedMaterial;
+            isCollied = false;
+            isPickedObject = true;
+        }else if (Input.GetKeyDown(KeyCode.Return) && SelectedObject != null && isPickedObject)
+        {
+            Debug.Log("........");
+            SelectedObject.transform.SetParent(null);
+            SelectedObject.GetComponent<MeshRenderer>().material = NormalMaterial;
+            isPickedObject = false;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -66,15 +83,52 @@ public class playerMovment : MonoBehaviour
 
         if (collision.gameObject.CompareTag("food")) //collision.gameObject.name == "foodToEat"
         {
-            
+            FillBar.fillAmount = FillBar.fillAmount + .35f;
+            if(FillBar.fillAmount == 1)
+            {
+                CompletePanel.SetActive(true);
+                Invoke("NextLevel", 2f);
+            }
             Debug.Log("collied with 2 food");
             Destroy(collision.gameObject);
             Health++;
-       }
+        }
+        if (collision.gameObject.CompareTag("trap"))
+        {
+            Destroy(collision.gameObject);
+            TrapedPanel.SetActive(true);
+            Invoke("Restart", 2f);
+        }
+        if (collision.gameObject.CompareTag("moveable"))
+        {
+            isCollied = true;
+            SelectedObject = collision.gameObject;
+        }
 
     }
-
-
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("moveable"))
+        {
+            isCollied = false;
+            SelectedObject = null;
+        }
+    }
+    public void NextLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void Home()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
 
 
 }
